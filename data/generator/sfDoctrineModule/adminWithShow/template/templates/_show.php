435797d3bbadf4acdd68583be923f1753ccb4f90
pcahard <pcahard@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-12-18 14:03:35 -0600

    replace table with div for the show view
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineAdminGeneratorWithShowPlugin/trunk@14190 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfDoctrineModule/adminWithShow/template/templates/_show.php
+++ data/generator/sfDoctrineModule/adminWithShow/template/templates/_show.php
@@ -1,19 +1,28 @@
-<table>
-  <tbody>
+[?php include_stylesheets_for_form($form) ?]
+[?php include_javascripts_for_form($form) ?]
+
+<div class="sf_admin_form">
 [?php foreach ($configuration->getFormFields($form, 'show') as $fieldset => $fields): ?]
+
+<fieldset id="sf_fieldset_[?php echo preg_replace('/[^a-z0-9_]/', '_', strtolower($fieldset)) ?]">
+  [?php if ('NONE' != $fieldset): ?]
+    <h2>[?php echo __($fieldset, array(), '<?php echo $this->getI18nCatalogue() ?>') ?]</h2>
+  [?php endif; ?]
+  
   [?php foreach ($fields as $name => $field): ?]
-  <tr>
+    <div class="sf_admin_form_row">
     [?php $attributes = $field->getConfig('attributes', array()); ?]
 	[?php if ($field->isPartial()): ?]
-      [?php include_partial('<?php echo $this->getModuleName() ?>/'.$name, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
+      [?php include_partial('<?php echo $this->getModuleName() ?>/'.$name, array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>, 'form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
     [?php elseif ($field->isComponent()): ?]
-      [?php include_component('<?php echo $this->getModuleName() ?>', $name, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
+      [?php include_component('<?php echo $this->getModuleName() ?>', $name, array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>, 'form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
     [?php else: ?]
-       <th>[?php echo $field->getConfig('label') ?]:</th>
-	   <td>[?php echo $form->getObject()->get($name) ?]</td>
+       <label>[?php echo $field->getConfig('label') ?]:</label>
+	   [?php echo $form->getObject()->get($name) ?]
     [?php endif; ?]
-  </tr>
+    </div>
   [?php endforeach; ?]
+</fieldset>  
+
 [?php endforeach; ?]
-  </tbody>
-</table>
+</div>
